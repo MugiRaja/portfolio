73066fba659ff63758bd0f205737a331a7e16f32
MugiRaja <rmugi2463@gmail.com>
2025-05-28 10:06:32 +0530

    new build test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,8 +7,14 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: true,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    ignores: ["**/node_modules/**", ".next/**", "out/**"],
+  },
+  ...compat.extends("next/core-web-vitals"),
+];
 
 export default eslintConfig;
