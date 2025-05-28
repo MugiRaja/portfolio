d9b0e60053d146dc729e4158332874b837136a26
MugiRaja <rmugi2463@gmail.com>
2025-05-28 09:42:52 +0530

    today

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <div className="w-full h-svh bg-gradient-to-b from-black to-red-950">
-  <nav className='w-full h-10 '>
-  </nav>
+  
+
+    <h1 className='text-white text-8xl font-extrabold'>Mukilan Rajaram</h1>
+  
 </div>
 
   )
